11419e525dfe83a1e137343ad0a47dffb63c59a8
Cristian <cristian.coppari@gmail.com>
2023-04-27 00:27:00 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["airbnb-base", "plugin:prettier/recommended"],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["prettier"],
+    rules: {
+        "import/order": [
+            "warn",
+            {
+                groups: [
+                    "type",
+                    "builtin",
+                    "object",
+                    "external",
+                    "internal",
+                    "parent",
+                    "sibling",
+                    "index",
+                ],
+                pathGroups: [
+                    {
+                        pattern: "~/**",
+                        group: "external",
+                        position: "after",
+                    },
+                ],
+                "newlines-between": "always",
+            },
+        ],
+    },
+};
